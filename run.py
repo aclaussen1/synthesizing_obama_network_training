cb5b5e82195a907d6b5f7201faa13e8f4d283e97
Alexander Claussen <aclaussen@ymail.com>
2017-11-22 19:04:05 -0500

    f

--- run.py
+++ run.py
@@ -129,7 +129,7 @@ class Speech(TFBase):
       output_w = tf.get_variable("output_w", [args.rnn_size, self.dimout])
       output_b = tf.get_variable("output_b", [self.dimout])
 
-    inputs = tf.split(self.input_data, 1, args.seq_length)
+    inputs = tf.split(self.input_data, args.seq_length, 1)
     inputs = [tf.squeeze(input_, [1]) for input_ in inputs]
 
     outputs, states = tf.nn.seq2seq.rnn_decoder(inputs, self.initial_state, self.network, loop_function=None, scope='rnnlm')
